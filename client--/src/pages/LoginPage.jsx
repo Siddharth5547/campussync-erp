18b32a515286071ce9b9cc3245c34e7198016555
Siddharth5547 <siddharthbharti0135@gmail.com>
2026-07-06 14:22:02 +0530

    Create login page

---
+++ client--/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center text-white">
+      <h1 className="text-5xl font-bold">
+        Login Page
+      </h1>
+    </div>
+  );
+}
+
+export default LoginPage;
